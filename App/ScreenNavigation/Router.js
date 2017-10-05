8ed8e0532fffb605b52b29e69761d3dd230a8d2f
classic <amit@Shuchi.local>
2017-10-05 12:50:54 +0530

    fix

--- App/ScreenNavigation/Router.js
+++ App/ScreenNavigation/Router.js
@@ -8,41 +8,10 @@ import Dashboard from '../Dashboard/Dashboard';
 import Paypal from '../Registration/Paypal';
 import ForgotPassword from '../Registration/ForgotPassword';
 import AddFriend from '../AddFriend/AddFriend';
-// import Recent from '../Upcomings/Recent';
-// import Upnext from '../Upcomings/Upnext';
 import Upcoming from '../Upcomings/Upcomings';
 import Calendars from '../Calendar/Calendar';
 import SendGift from '../SendGift/SendGift';
-// const Routes =   {
-//   RECECNT: { screen : Recent,navigationOptions: {
-//       tabBarLabel: 'Recent Birthdays'
-//     }},
-//   UPNEXT : { screen : Upnext,navigationOptions: {
-//       tabBarLabel: "Up Next"
-//     }},
-//   UPCOMING :{ screen : Upcoming,navigationOptions: {
-//       tabBarLabel: "Upcoming Birthays"
-//     }},
-// };
-// const TabConfig = {
-//   stateName: 'MainNavigation',
-//   tabBarPosition: 'top',
-//   animationEnabled: true,
-//   tabBarOptions: {
-//     activeTintColor: '#DC6361',
-//     inactiveTintColor: 'black',
-//     upperCaseLabel: false,
-//     labelStyle: {
-//       fontSize: 10,
-//       width: '100%',
-//     },style: {
-//     backgroundColor: '#ffffff',
-//     borderTopWidth: 1,
-//     borderTopColor: 'gray',
-//   }
-//   },
-// };
-// register all screens of the app (including internal ones)
+
 export const screenRoute = (SignIn) => {
   return StackNavigator({
     LOG_IN:{screen:Login},
